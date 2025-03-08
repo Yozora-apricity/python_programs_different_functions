2d6946c90798a5d56ed03f02d1a54f13deaa4ff3
Gerald <geraldtrogado@gmail.com>
2025-03-08 21:27:07 +0800

    Added a function where it adds the two values and prints the sum

--- print_sum_two_numbers.py
+++ print_sum_two_numbers.py
@@ -1,2 +1,7 @@
 #1. Ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 #2. Print the sum of the two numbers
+result = num1 + num2
+print(result)
