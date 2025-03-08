9c05d40933603c67ca95173773c797849fef4c74
Gerald <geraldtrogado@gmail.com>
2025-03-08 21:35:32 +0800

    Added the pseudo code and ask user to input two numbers and print the product of the two numbers.

---
+++ print_product_of_two_numbers.py
@@ -0,0 +1,7 @@
+#1. Ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#2. Print the product of the two numbers
+result = num1 * num2
+print(result)
