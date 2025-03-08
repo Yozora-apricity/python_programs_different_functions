f1927a01402a36f869d963b1791406f7aa9b6306
Gerald <geraldtrogado@gmail.com>
2025-03-08 20:59:56 +0800

    Ask user to enter a number and check if the numbers are equal and print

--- print_equal_numbers.py
+++ print_equal_numbers.py
@@ -1,2 +1,9 @@
 #1.Ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 #2 Check if the numbers are equal and print the value
+if num1 == num2:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
