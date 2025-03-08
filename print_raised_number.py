5251b8fba6201b124ada297217653a063c1edec4
Gerald <geraldtrogado@gmail.com>
2025-03-08 22:09:07 +0800

    Print the result of the raised number to the power of the second number.

--- print_raised_number.py
+++ print_raised_number.py
@@ -1,3 +1,7 @@
 #1. Ask user to input 2 numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
+#2. Print the raised number of the two numbers
+result = num1 ** num2
+print(result)
