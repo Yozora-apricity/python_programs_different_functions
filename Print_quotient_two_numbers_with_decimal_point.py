efccd6c84fa4b9d716103d6da1147fa96b9944ec
Gerald <geraldtrogado@gmail.com>
2025-03-09 21:23:53 +0800

    Just added some extra details to the comment

--- Print_quotient_two_numbers_with_decimal_point.py
+++ Print_quotient_two_numbers_with_decimal_point.py
@@ -1,4 +1,4 @@
-#1. Ask user to input 2 numbers
+#1. Ask user to input 2 numbers and store them in variables num1 and num2
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
