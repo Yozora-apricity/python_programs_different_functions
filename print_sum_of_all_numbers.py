7eab41d76f7234d3a596ea42ebc5bda57cbebf66
Gerald <geraldtrogado@gmail.com>
2025-03-09 12:07:43 +0800

    Changed the written pseudocode and added the code that prints and sum all the numbers from 1 to 10.

--- print_sum_of_all_numbers.py
+++ print_sum_of_all_numbers.py
@@ -1,2 +1,17 @@
 # 1. Ask user to input 10 numbers
-# 2. Print the sum of all the numbers
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num4 = int(input("Enter number 4: "))
+num5 = int(input("Enter number 5: "))
+num6 = int(input("Enter number 6: "))
+num7 = int(input("Enter number 7: "))
+num8 = int(input("Enter number 8: "))
+num9 = int(input("Enter number 9: "))
+num10 = int(input("Enter number 10: "))
+
+# 2. Adds all the numbers
+sum_of_all_numbers = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
+
+# 3. Print the sum of all numbers
+print("The sum of all numbers is:", sum_of_all_numbers)
