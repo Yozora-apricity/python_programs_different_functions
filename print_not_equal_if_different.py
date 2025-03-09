f3d6a8abd321f60dd449d91f2802d11f3fda6156
Gerald <geraldtrogado@gmail.com>
2025-03-09 21:03:38 +0800

    Added user input prompts for the user to 3 files to be read in and processed.

---
+++ print_not_equal_if_different.py
@@ -0,0 +1,4 @@
+#1. Ask the user to input 2 numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,2 +1,5 @@
 # 1. Ask user to input 2 numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Print the smaller number.
