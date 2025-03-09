1401bb3c2e93d8b97e770fc9232982178486caa4
Gerald <geraldtrogado@gmail.com>
2025-03-09 21:14:05 +0800

    Check if the input is smaller and print the smaller number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -2,4 +2,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-# Print the smaller number.
+# Check if which among the two numbers is smaller and print the smaller number.
+if num1 < num2:
+    print(num1)
+else:
+    print(num2)
