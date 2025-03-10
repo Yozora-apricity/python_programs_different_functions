080feb1fe05b6b4bd1d1362d5ba2fbb7e8137c39
Gerald <geraldtrogado@gmail.com>
2025-03-10 12:00:35 +0800

    Added user input prompts for entering ten numbers in count_even_numbers.py and subtract_first_from_rest.py

---
+++ count_even_numbers.py
@@ -0,0 +1,11 @@
+#1. Ask a number from the user 10 times
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter a number: "))
+num5 = int(input("Enter a number: "))
+num6 = int(input("Enter a number: "))
+num7 = int(input("Enter a number: "))
+num8 = int(input("Enter a number: "))
+num9 = int(input("Enter a number: "))
+num10 = int(input("Enter a number: "))
---
+++ subtract_first_from_rest.py
@@ -0,0 +1,13 @@
+#1. Ask user to input 10 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the fifth number: "))
+num6 = int(input("Enter the sixth number: "))
+num7 = int(input("Enter the seventh number: "))
+num8 = int(input("Enter the eighth number: "))
+num9 = int(input("Enter the ninth number: "))
+num10 = int(input("Enter the tenth number: "))
+
+#2. Subtract the first number from each of the other numbers
