00c8148479e7bbad63bdf840fc1810d51327bf58
Gerald <geraldtrogado@gmail.com>
2025-03-10 13:24:56 +0800

    Added the while function and print function to the code

--- print_numbers_between.py
+++ print_numbers_between.py
@@ -1,3 +1,8 @@
 # Ask user to input 3 0numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+# Print numbers between num1 and num2
+while num1 <= num2:
+    print(num1)
+    num1 += 1
