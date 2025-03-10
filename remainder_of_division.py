4908d18faf2e39d47287f0bf462828b4f94acb31
Gerald <geraldtrogado@gmail.com>
2025-03-10 11:37:23 +0800

    Added calculation and display of remainder in remainder_of_division.py and quotient_without_division.py

--- remainder_of_division.py
+++ remainder_of_division.py
@@ -1,3 +1,9 @@
 #1. Ask user to enter two numbers
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
+
+#2. Find the remainder of the two numbers
+result = num1 % num2
+
+#3. Display the result
+print(result)
