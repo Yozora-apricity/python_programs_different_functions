c95a4cf6230ab169e87067a52501d41bde068de4
Gerald <geraldtrogado@gmail.com>
2025-03-10 13:35:43 +0800

    Starts from num1 to another number

--- print_numbers_between.py
+++ print_numbers_between.py
@@ -2,6 +2,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+num1 += 1
 # Print numbers between num1 and num2
 while num1 <= num2:
     print(num1)
