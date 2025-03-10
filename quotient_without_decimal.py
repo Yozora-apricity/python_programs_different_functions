d295a631de0e960a747a08782c35e4dfe781d304
Gerald <geraldtrogado@gmail.com>
2025-03-10 11:30:41 +0800

    Add user input prompts and calculation for quotient without decimal in quotient_without_decimal.py

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,2 +1,8 @@
 #1. Ask user to enter two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 #2. Find the quotient of the two numbers without decimal
+
+result = num1 // num2
+print(result)
