8b910f910924efdd9b75b1937fd169d96ebda02b
Gerald <geraldtrogado@gmail.com>
2025-03-10 13:30:37 +0800

    Added comment where it explains the function of the code

--- exclude_numbers_ending_0_5.py
+++ exclude_numbers_ending_0_5.py
@@ -1,6 +1,8 @@
 num = 0
 
+#Loop through numbers from 0 to 100
 while num < 100:
+#Check if number is not divisible by 10 and 5
     if num % 10 != 0 and num % 5 != 0:
         print(num)
     num += 1
