42ab18077a4366eec7428afdf1d7750e9d6f322b
Gerald <geraldtrogado@gmail.com>
2025-03-10 12:05:33 +0800

    Added functionality to count and the print number of even numbers from user input

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -9,3 +9,12 @@ num7 = int(input("Enter a number: "))
 num8 = int(input("Enter a number: "))
 num9 = int(input("Enter a number: "))
 num10 = int(input("Enter a number: "))
+
+#2. Count the number of even numbers
+even_count = 0
+for i in range(1, 11):
+    if i % 2 == 0:
+        even_count += 1
+
+#3. Print the number of even numbers
+print("The number of even numbers is", even_count)
