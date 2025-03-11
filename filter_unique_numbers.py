5ea9daeb0b7572b410c75cd74ba76b75c63d4f61
Gerald <geraldtrogado@gmail.com>
2025-03-11 23:48:26 +0800

    Added logic to filter unique numbers and store them in a new list

--- filter_unique_numbers.py
+++ filter_unique_numbers.py
@@ -12,3 +12,12 @@ num10 = int(input("Enter tenth number: "))
 
 #2. Store the numbers in a list
 numbers = [num1, num2, num3, num4, num5, num6, num7, num8, num9, num10]
+
+a = []
+
+for num in a:
+    if num not in a:
+        for i in range(10):
+            if a[i] == "":
+                a[i] = num
+                break
