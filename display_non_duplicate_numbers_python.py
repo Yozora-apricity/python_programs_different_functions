6400540ec10f6c9e86e64fee6dd9f27641e2849a
Gerald <geraldtrogado@gmail.com>
2025-03-11 11:29:12 +0800

    Added user input for ten numbers and check for non-duplicate entries

---
+++ display_non_duplicate_numbers_python.py
@@ -0,0 +1,33 @@
+# 1. Ask user to input 10 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
+num5 = int(input("Enter fifth number: "))
+num6 = int(input("Enter sixth number: "))
+num7 = int(input("Enter seventh number: "))
+num8 = int(input("Enter eighth number: "))
+num9 = int(input("Enter ninth number: "))
+num10 = int(input("Enter tenth number: "))
+
+#2. check if the inputted numbers are the same
+if num1 != num2 != num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num1)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num2)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num3)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num4)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num5)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num6)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num7)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num8)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num9)
+elif num1 != num2 == num3 != num4 != num5 != num6 != num7 != num8 != num9 != num10:
+    print(num10)
