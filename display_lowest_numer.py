a9146c250274668e9d89c33ccde5449ca45ef93f
Gerald <geraldtrogado@gmail.com>
2025-03-12 10:43:17 +0800

    Fixed a lot of error in indentation in line 10 to line 23

--- display_lowest_numer.py
+++ display_lowest_numer.py
@@ -4,22 +4,22 @@
 # If input is not a valid number, break the loop
 # Display the lowest number if any valid number was entered
 
-def find_lowest_numbers():
+def find_lowest_number():
     lowest = None
-    
-while True:
-    try:
-        num = int(input("Enter a number: "))
-        
-    if lowest is None or num < lowest:
-        lowest = num
-    
-    except ValueError:
-    break
 
-    if lowest is None:
-    print("The lowest number entered was: {lowest}")
+    while True:
+        try:
+            num = int(input("Enter a number (or any non-number to stop): "))
+
+            if lowest is None or num < lowest:
+                lowest = num
+
+        except ValueError:
+            break
+
+    if lowest is not None:
+        print(f"The lowest number entered was: {lowest}")
     else:
-    print("No valid numbers were entered.")
-    
-find_lowest_numbers()
+        print("No valid numbers were entered.")
+
+find_lowest_number()
