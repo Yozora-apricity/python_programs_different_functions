f06db2687fbb5313cb8425f84f222d64ac6645e3
Gerald <geraldtrogado@gmail.com>
2025-03-12 13:36:56 +0800

    Set up the intial values as well as add the while loop to continuous ask user to input numbers

--- average.py
+++ average.py
@@ -1,3 +1,17 @@
 #Set up initial values
+total = 0
+count = 0
 # Loop to get user input continuously
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        total += num
+        count += 1
+    except ValueError:
+        break
 # Calculate and display the average
+if count > 0:
+    average = total / count
+    print("Average of entered numbers:", average)
+else:
+    print("No valid numbers entered.")
