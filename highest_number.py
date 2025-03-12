e01b3754ed235be820872dfe16f8b52d9d349afd
Gerald <geraldtrogado@gmail.com>
2025-03-12 13:31:55 +0800

    Fixed an error on line 7 which is not closed

--- highest_number.py
+++ highest_number.py
@@ -1,4 +1,4 @@
-# Set up the highest number variable
+# Initialize the highest number variable
 highest = None
 
 # Continuously ask for input until an invalid entry is given
@@ -6,12 +6,13 @@ while True:
     try:
         num = int(input("Enter a number: "))
         
-    # Update the highest number if needed
+        # Update the highest number if needed
         if highest is None or num > highest:
             highest = num
-        except ValueError:
+    except ValueError:
         break
-    # Display the highest number entered
+
+# Display the highest number entered
 if highest is not None:
     print("The highest number entered was:", highest)
 else:
