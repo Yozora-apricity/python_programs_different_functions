79328ab2137cc010d7f03360c5fb0c5bc3ad415f
Gerald <geraldtrogado@gmail.com>
2025-03-12 10:56:46 +0800

    Done with the added flow and loop function of the code when sorting and inputting

--- sorted_number.py
+++ sorted_number.py
@@ -1,3 +1,16 @@
 # Ask user to input a number
 # Sort the number
 #Display number
+
+while True:
+try:
+        
+        num = int(input("Enter a number: "))
+        numbers += [num]
+        
+except ValueError:
+    break
+    
+numbers.sort()
+
+print(numbers)
