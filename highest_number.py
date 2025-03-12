410c245a1069d656c86d062ea32482c4967c1a6f
Gerald <geraldtrogado@gmail.com>
2025-03-12 13:29:30 +0800

    added while loop when asking user to input numbers and update the number when here is a higher number inputted as well as th print or display function

--- highest_number.py
+++ highest_number.py
@@ -1,4 +1,18 @@
 # Set up the highest number variable
+highest = None
+
 # Continuously ask for input until an invalid entry is given
-# Update the highest number if needed
-# Display the highest number entered
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        
+    # Update the highest number if needed
+        if highest is None or num > highest:
+            highest = num
+        except ValueError:
+        break
+    # Display the highest number entered
+if highest is not None:
+    print("The highest number entered was:", highest)
+else:
+    print("No valid numbers were entered.")
