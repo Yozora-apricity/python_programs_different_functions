6cae2c2ba29975bd73033a14b589f78b22d23d75
Gerald <geraldtrogado@gmail.com>
2025-03-12 13:05:26 +0800

    Created input function for most frequent and duplicate.py

---
+++ most_frquent.py
@@ -0,0 +1,8 @@
+def most_frequent():
+    # Store number frequencies  
+    freq = {}  
+    
+    # Take user input  
+    while True:
+        try:
+            num = int(input("Enter a number: "))
