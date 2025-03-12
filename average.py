35d8fa583f1ac786efca83d69811b787a3a6e1b6
Gerald <geraldtrogado@gmail.com>
2025-03-12 13:23:58 +0800

    Added pseudo code for 2 python files

---
+++ average.py
@@ -0,0 +1,3 @@
+#Set up initial values
+# Loop to get user input continuously
+# Calculate and display the average
---
+++ highest_number.py
@@ -0,0 +1,4 @@
+# Set up the highest number variable
+# Continuously ask for input until an invalid entry is given
+# Update the highest number if needed
+# Display the highest number entered
