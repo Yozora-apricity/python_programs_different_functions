c7429790940c977093169838f23b756179e25de1
Gerald <geraldtrogado@gmail.com>
2025-03-12 10:39:34 +0800

    Created a function to find the lowest number from user input

--- display_lowest_numer.py
+++ display_lowest_numer.py
@@ -4,3 +4,20 @@
 # If input is not a valid number, break the loop
 # Display the lowest number if any valid number was entered
 
+def find_lowest_numbers():
+    lowest = None
+    
+while True:
+try:
+    num = int(input("Enter a number: "))
+        
+if lowest is None or num < lowest:
+        lowest = num
+    
+except ValueError:
+break
+
+if lowest is None:
+    print("The lowest number entered was: {lowest}")
+else:
+    print("No valid numbers were entered.")
